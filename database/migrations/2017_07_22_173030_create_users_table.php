8c15cd493d4d26abb2add9ea53d42bb20cad2e8b
colnan007 <colnan007@outlook.com>
2017-07-22 14:54:10 -0400

    Crear Modelo usuario y sus relaciones y migraciones

--- database/migrations/2017_07_22_173030_create_users_table.php
+++ database/migrations/2017_07_22_173030_create_users_table.php
@@ -16,10 +16,16 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('username');
+            $table->integer('role_id')->unsigned();
+            $table->string('phone');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('roles');
+
         });
     }
 

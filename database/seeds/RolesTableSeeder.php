aa07f91cd4a43752611b621262bca65d31d76469
colnan007 <colnan007@outlook.com>
2017-07-22 14:55:23 -0400

    Crear modelo role y migraciones, relaciones y seeder

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Role;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = [
+        	['id' => 1, 'description' => 'Administrator'],
+        	['id' => 2, 'description' => 'Data Entry'],
+        	['id' => 3, 'description' => 'Superviser']
+        ];
+
+        foreach($roles as $role)
+        {
+        	Role::create($role);
+        }
+    }
+}
